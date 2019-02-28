2bb171fdef4b3a63db3b739c80464eb30be79ab6
Quantoxis <a.richardson92@live.co.uk>
2019-02-28 13:41:44 +0000

    Update GetRunnableClass.java

--- Communication/src/communication/GetRunnableClass.java
+++ Communication/src/communication/GetRunnableClass.java
@@ -1,18 +1,19 @@
+public class GetRunnableClass implements Runnable {
 
-package communication;
-
-public class GetRunnableClass implements Runnable{
     
-    Test  test;
+    Test test;
     
-    public void GetRunnableClass(Test test){
-        this.test = test;
+    public GetRunnableClass(Test test)
+    {
+         this.test = test;
     }
     
-    public void run(){
+    public void run() {
         System.out.println("Thread 2 is running!");
-        Test test = new Test();
+        
         test.getValue();
         System.out.println("The value is: " + test.getValue());
     }
+
 }
+
