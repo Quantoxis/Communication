b983817d69b8424f232e57869fccbee999f11c7c
Quantoxis <a.richardson92@live.co.uk>
2019-02-28 12:38:57 +0000

    Update and rename Thread1.java to SetRunnableClass.java

--- Communication/src/communication/SetRunnableClass.java
+++ Communication/src/communication/SetRunnableClass.java
@@ -5,11 +5,7 @@
  */
 package communication;
 
-/**
- *
- * @author Andrew
- */
-public class Thread1 implements Runnable{
+public class SetRunnableClass implements Runnable{
     
     public void run(){
         System.out.println("Hello from a thread!");
