ea12a457057bea195d327fbca6025f8116f940ce
Quantoxis <a.richardson92@live.co.uk>
2019-02-28 13:44:29 +0000

    Update Communication.java

--- Communication/src/communication/Communication.java
+++ Communication/src/communication/Communication.java
@@ -4,9 +4,11 @@ package communication;
 public class Communication {
 
     public static void main(String[] args) {
-        
-    SetRunnableClass st1 = new SetRunnableClass();
-    GetRunnableClass gt1 = new GetRunnableClass();
+    
+    Test test = new Test();
+    
+    SetRunnableClass st1 = new SetRunnableClass(test);
+    GetRunnableClass gt1 = new GetRunnableClass(test);
     Thread t = new Thread(st1);
     Thread t2 = new Thread(gt1);
     
